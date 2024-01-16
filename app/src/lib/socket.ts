66f0abe250fa92a03a2736443ef6906d80b1f2c0
Devr <bhargavj2804@pm.me>
2024-01-16 23:02:17 +0530

    Update socket.ts

--- app/src/lib/socket.ts
+++ app/src/lib/socket.ts
@@ -1,6 +1,5 @@
-import { browser, dev } from "$app/environment"
 import {io} from "socket.io-client"
 
-export const socket= io(dev&&browser?'https://refactored-space-goldfish-6qj5769r7624r4j-3000.app.github.dev/':'https://share-clam-70.deno.dev/',{
+export const socket= io('https://socket-sveltekit.deno.dev/',{
 		transports:["websocket"]
 	})
